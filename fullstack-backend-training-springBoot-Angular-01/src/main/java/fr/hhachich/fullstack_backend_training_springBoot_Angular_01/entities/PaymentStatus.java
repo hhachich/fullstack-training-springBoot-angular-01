7fece67fdce5531a8fabdfd04ba5a8611b65abbc
hhachich <h.hachichou@gmail.com>
2024-11-07 09:18:01 +0100

    update

--- fullstack-backend-training-springBoot-Angular-01/src/main/java/fr/hhachich/fullstack_backend_training_springBoot_Angular_01/entities/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.hhachich.fullstack_backend_training_springBoot_Angular_01.entities;
-
-public enum PaymentStatus {
-    CREATED, VALIDATED, REJECTED
-}
--- fullstack-backend-training-springBoot-Angular-01/src/main/java/fr/hhachich/fullstack_backend_training_springBoot_Angular_01/entities/PaymentType.java
+++
@@ -1,5 +0,0 @@
-package fr.hhachich.fullstack_backend_training_springBoot_Angular_01.entities;
-
-public enum PaymentType {
-    CASH, CHECK, TRANSFER, DEPOSIT
-}
